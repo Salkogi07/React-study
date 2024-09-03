01d70c446a4689ab787d2c3d09142575f02658a8
kogisal <kogisal099@gmail.com>
2024-09-03 20:11:43 +0900

    9_3

--- shop/src/data.js
+++ shop/src/data.js
@@ -3,19 +3,22 @@ let data = [
         id : 0,
         title : "white and black",
         content : "from France",
-        price : 120000
+        price : 120000,
+        img : "/shoe1.png"
     },
     {
         id : 1,
         title : "red wine",
         content : "from Seoul",
-        price : 110000
+        price : 110000,
+        img : "/shoe2.png"
     },
     {
         id : 2,
         title : "Grey Jordan",
         content : "from United States",
-        price : 130000
+        price : 130000,
+        img : "/shoe3.png"
     },
 ]
 export default data;
